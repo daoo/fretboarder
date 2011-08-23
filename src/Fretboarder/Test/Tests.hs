60b7e3e090b86abbe6981b89da119c1cb586ba38
daoo <daoo314@gmail.com>
2011-08-23 18:36:20 +0200

    New tests and fixes

--- src/Fretboarder/Test/Tests.hs
+++ src/Fretboarder/Test/Tests.hs
@@ -6,9 +6,22 @@ module Tests where
 
 import Test.QuickCheck
 
-import INote
-import Note
+import Fretboarder.Parser.String
+
+import Fretboarder.Guitar.INote
+import Fretboarder.Guitar.Note
 
 fromToINote :: INote -> Bool
 fromToINote i = i == (toINote $ fromINote i)
+
+equalINotes :: Bool
+equalINotes = all (\ (a, b) -> toINote a == toINote b) notes
+  where
+    notes =
+      [ (Note A 4 Sharp, Note B 4 Flat)
+      , (Note B 4 Natural, Note C 5 Flat)
+      , (Note B 4 Sharp, Note C 5 Natural)
+      , (Note E 4 Natural, Note F 4 Flat)
+      , (Note E 4 Sharp, Note F 4 Natural)
+      ]
 

b946be2003ba82321f7fbdebaa94cbf96c6c7654
daoo <daoo314@gmail.com>
2011-09-09 10:52:44 +0200

    Implemted readOffsets using levenshtein distance

--- src/Fretboarder/Parser/String.hs
+++ src/Fretboarder/Parser/String.hs
@@ -30,5 +30,38 @@ readAccidental '#' = Sharp
 readAccidental 'b' = Flat
 readAccidental _   = Natural
 
+levenshtein :: String -> String -> Int
+levenshtein s t = d !! (length s) !! (length t) 
+  where 
+    d = [[distance m n|n<-[0..length t]]|m<-[0..length s]]
+
+    distance i 0 = i
+    distance 0 j = j
+    distance i j = minimum [ d!!(i-1)!!j+1
+                           , d!!i!!(j-1)+1
+                           , d!!(i-1)!!(j-1) + (if s!!(i-1)==t!!(j-1) then 0 else 1)]
+
 readOffsets :: String -> [Offset]
-readOffsets "dorian" = dorianMode
+readOffsets str = snd $ head lst
+  where
+    lst = sort $ map f offsets
+
+    f :: (String, [Offset]) -> (Int, [Offset])
+    f (scale, offsets) = (levenshtein scale str, offsets)
+
+    offsets :: [(String, [Offset])]
+    offsets = [ ("major", majorScale)
+              , ("minor", minorScale)
+
+              , ("harmonic minor", harmonicMinor)
+              , ("melodic minor", melodicMinor)
+
+              , ("blues", bluesScale)
+
+              , ("ionian", ionianMode)
+              , ("dorian", dorianMode)
+              , ("phrygian", phrygianMode)
+              , ("lydian", lydianMode)
+              , ("mixolydian", mixolydianMode)
+              , ("aeolian", aeolianMode)
+              , ("locrian", locrianMode) ]

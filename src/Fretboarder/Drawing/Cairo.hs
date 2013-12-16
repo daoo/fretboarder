11dbbb8500080fd18a770b5f5d1fba337b2d6b63
daoo <daoo314@gmail.com>
2013-12-16 10:15:46 +0100

    Clean up

--- src/Fretboarder/Drawing/Cairo.hs
+++ src/Fretboarder/Drawing/Cairo.hs
@@ -9,18 +9,12 @@ import Fretboarder.Guitar.Note
 import Fretboarder.Guitar.Scale
 import qualified Graphics.Rendering.Cairo as C
 
-data Color = Color !Double !Double !Double
-data Point = Point !Double !Double
-data Line  = Line !Point !Point
+data Color = Color {-# UNPACK #-} !Double {-# UNPACK #-} !Double {-# UNPACK #-} !Double
+data Point = Point {-# UNPACK #-} !Double {-# UNPACK #-} !Double
+data Line  = Line {-# UNPACK #-} !Point {-# UNPACK #-} !Point
 
-instance Num Point where
-  Point x1 y1 + Point x2 y2 = Point (x1+x2) (y1+y2)
-
-  (*) = undefined
-  (-) = undefined
-  abs = undefined
-  signum = undefined
-  fromInteger = undefined
+(.+) :: Point -> Point -> Point
+Point x1 y1 .+ Point x2 y2 = Point (x1+x2) (y1+y2)
 
 tangoColors :: [Color]
 tangoColors =
@@ -34,8 +28,8 @@ tangoColors =
   , Color 0.533333333333333 0.541176470588235 0.521568627450980
   ]
 
-(++++) :: Line -> Point -> Line
-Line a b ++++ p = Line (a + p) (b + p)
+(.+.) :: Line -> Point -> Line
+Line a b .+. p = Line (a .+ p) (b .+ p)
 
 pi2 :: Double
 pi2 = pi * 2.0
@@ -52,7 +46,7 @@ mkLine (Line (Point x1 y1) (Point x2 y2)) = C.moveTo x1 y1 >> C.lineTo x2 y2
 mkLines :: Int -> Point -> Line -> C.Render ()
 mkLines !count !delta !line = go 0 line
   where
-    go !i !acc | i < count = mkLine acc >> go (i+1) (acc ++++ delta)
+    go !i !acc | i < count = mkLine acc >> go (i+1) (acc .+. delta)
                | otherwise = return ()
 
 getColor :: INote -> [Scale] -> Maybe Color
@@ -66,15 +60,15 @@ drawFretboard w h fb scales = do
   setColor fgColor
 
   C.setLineWidth (lineWidth * 10.0)
-  mkLine $ Line topLeft (Point px (py + bh))
+  mkLine $ Line topleft botleft
   C.setLineWidth lineWidth
 
-  mkLines frets   (Point fretw 0) (Line (topLeft + Point fretw 0) (topLeft + Point fretw bh))
-  mkLines strings (Point 0 freth) (Line topLeft (topLeft + Point bw 0))
+  mkLines frets   (Point fretw 0) (Line (topleft .+ Point fretw 0) (botleft .+ Point fretw 0))
+  mkLines strings (Point 0 freth) (Line topleft topright)
   C.stroke
 
   drawInlays inlays
-  drawFrets (tuning fb)
+  drawStrings (tuning fb)
   where
     lineWidth = 1.0
     fgColor   = Color 0 0 0
@@ -85,19 +79,31 @@ drawFretboard w h fb scales = do
 
     inlays = [(3, 1), (5, 1), (7, 1), (9, 1), (12, 2), (15, 1), (17, 1), (19, 1), (21, 1)]
 
-    topLeft@(Point px py) = Point 50 20
+    marginx = 20
+    marginy = 20
 
-    bw = w - 2 * px
-    bh = h - 2 * py
+    boardw = w - 2 * marginx
+    boardh = h - 2 * marginy
 
-    fretw  = bw / fromIntegral frets
-    freth  = bh / fromIntegral (strings - 1)
-    radius = (bh / fromIntegral strings) / 5
+    topleft  = Point marginx            marginy
+    topright = Point (marginx + boardw) marginy
+    botleft  = Point marginx            (marginy + boardh)
+    --botright = Point (marginx + boardw) (marginy + boardh)
 
-    fretx, frety :: Int -> Double
-    fretx 0 = px
-    fretx i = px + fretw / 2.0 + realToFrac (i - 1) * fretw
-    frety i = py + realToFrac i * freth
+    fretw  = boardw / realToFrac frets
+    freth  = boardh / realToFrac (strings - 1)
+    radius = (boardh / realToFrac strings) / 5.0
+
+    fretx1 = marginx
+    fretx2 = marginx + fretw / 2.0
+    frety1 = marginy
+
+    fretx :: Int -> Double
+    fretx 0 = marginx
+    fretx i = marginx + fretw / 2.0 + realToFrac (i - 1) * fretw
+
+    -- frety :: Int -> Double
+    -- frety i = marginy + realToFrac i * freth
 
     fillCircle point = mkCircle radius point >> C.fill
 
@@ -105,26 +111,31 @@ drawFretboard w h fb scales = do
     drawInlay :: (Int, Int) -> C.Render ()
     drawInlay (f, 1) = do
       let x = fretx f
-      fillCircle (Point x (py + bh / 2.0))
+      fillCircle (Point x (marginy + boardh / 2.0))
 
     drawInlay (f, 2) = do
       let x = fretx f
-      fillCircle (Point x (py + 3.0 * freth / 2.0))
-      fillCircle (Point x (py + 7.0 * freth / 2.0))
+      fillCircle (Point x (marginy + 3.0 * freth / 2.0))
+      fillCircle (Point x (marginy + 7.0 * freth / 2.0))
 
     drawInlay _ = return ()
 
     drawInlays = mapM_ drawInlay
 
-    drawFret i j n = case getColor n scales of
-      Just c  -> setColor c >> fillCircle (Point (fretx i) (frety j))
+    drawFret !x !y !n = case getColor n scales of
+      Just c  -> setColor c >> fillCircle (Point x y)
       Nothing -> return ()
 
-    drawFrets = goy 0
+    drawStrings :: [INote] -> C.Render ()
+    drawStrings = go 0 frety1
       where
-        goy  _ []        = return ()
-        goy !j (root:ys) = gox 0 >> goy (j+1) ys
+        go :: Int -> Double -> [INote] -> C.Render ()
+        go  _  _ []        = return ()
+        go !j !y (root:ys) = drawString y root >> go (j+1) (y+freth) ys
 
-          where
-            gox !i | i <= frets = drawFret i j (root+i) >> gox (i+1)
-                   | otherwise  = return ()
+    drawString :: Double -> INote -> C.Render ()
+    drawString !y !root = drawFret fretx1 y root >> go 1 fretx2
+      where
+        go :: Int -> Double -> C.Render ()
+        go !i !x | i <= frets = drawFret x y (root+i) >> go (i+1) (x+fretw)
+                 | otherwise  = return ()

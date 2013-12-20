2c8e18730bd99dfbdffe2d15b5ba33359ba0bf35
daoo <daoo314@gmail.com>
2013-12-20 14:43:30 +0100

    Clean up

--- src/ASCII.hs
+++ src/ASCII.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
 import Data.Attoparsec.Text
-import Data.List
 import Fretboarder.Drawing.ASCII
 import Fretboarder.Music.Fretboard
 import Fretboarder.Parser
@@ -9,8 +8,9 @@ import System.Environment
 import qualified Data.Text as T
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case parseOnly parseExprs (T.pack $ intercalate " " args) of
-    Left err    -> print err
-    Right exprs -> putStrLn $ asciiFretboard 23 ebgdae exprs
+main = getArgs >>= prg
+
+prg :: [String] -> IO ()
+prg args = case parseOnly parseExprs (T.pack $ unwords args) of
+  Left err    -> print err
+  Right exprs -> putStrLn $ asciiFretboard 23 ebgdae exprs

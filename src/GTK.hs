63f23eb19c33fbe396b99bc968e4a46e51e4f568
daoo <daoo314@gmail.com>
2013-12-20 13:45:47 +0100

    Remove reactive banana dependency
    Also clean up other unused dependencies.

--- src/GTK.hs
+++ src/GTK.hs
@@ -2,46 +2,32 @@ module Main (main) where
 
 import Control.Monad.IO.Class
 import Data.Attoparsec.Text
+import Data.IORef
 import Fretboarder.Drawing.Cairo
 import Fretboarder.Drawing.Expr
 import Fretboarder.Music.Fretboard
 import Fretboarder.Parser
 import Graphics.UI.Gtk hiding (Scale)
-import Reactive.Banana
-import Reactive.Banana.Frameworks
 import qualified Data.Text as T
 
 main :: IO ()
 main = initGUI >> setupWindow >>= widgetShowAll >> mainGUI
 
-colorGreen, colorRed :: Color
-colorGreen = Color 35328 57856 13312
-colorRed   = Color 61184 10496 10496
-
-setupNetwork :: (Maybe [Expr] -> IO ()) -> (Color -> IO ()) -> AddHandler String -> AddHandler () -> IO EventNetwork
-setupNetwork draw color estext esconf = compile $ do
-  etext <- fromAddHandler estext
-  econf <- fromAddHandler esconf
-  let (err, scale) = split $ (parseOnly parseExprs . T.pack) <$> etext
-
-      edraw = accumE Nothing $ union
-        (id <$ econf)
-        ((const . Just) <$> scale)
-
-      ecolor = accumE colorRed $ union
-        (const colorGreen <$ scale)
-        (const colorRed   <$ err)
-
-  reactimate $ draw <$> edraw
-  reactimate $ color <$> ecolor
+green, red :: Color
+green = Color 35328 57856 13312
+red   = Color 61184 10496 10496
 
 setWidgetBg :: WidgetClass self => self -> Color -> IO ()
 setWidgetBg e = widgetModifyBase e StateNormal
+
+type State = [Expr]
 
 setupWindow :: IO Window
 setupWindow = do
   window <- windowNew
   set window [ windowTitle := "Fretboarder" ]
+
+  state <- newIORef []
 
   vbox   <- vBoxNew False 0
   canvas <- drawingAreaNew
@@ -51,23 +37,21 @@ setupWindow = do
   boxPackEnd vbox canvas PackGrow 0
   boxPackStart vbox entry PackNatural 0
 
-  esTextChange <- newAddHandler
-  esConfigure  <- newAddHandler
-
-  network <- setupNetwork (drawScale canvas) (setWidgetBg entry) (fst esTextChange) (fst esConfigure)
-  actuate network
-
   _ <- on window deleteEvent $ liftIO mainQuit >> return False
-  _ <- on window configureEvent $ liftIO (snd esConfigure ()) >> return False
-  _ <- on entry editableChanged $ entryGetText entry >>= snd esTextChange
+  _ <- on window configureEvent $ liftIO (readIORef state >>= draw canvas) >> return False
+  _ <- on entry editableChanged $ entryGetText entry >>= text (draw canvas) (setWidgetBg entry) state
 
   return window
 
-drawScale :: DrawingArea -> Maybe [Expr] -> IO ()
-drawScale _ Nothing          = return ()
-drawScale canvas (Just exprs) = do
-  win  <- widgetGetDrawWindow canvas
-  (w, h) <- widgetGetSize canvas
+text :: ([Expr] -> IO ()) -> (Color -> IO ()) -> IORef State -> String -> IO ()
+text d c s t = case parseOnly parseExprs (T.pack t) of
+  Left _      -> writeIORef s []    >> c red   >> d []
+  Right exprs -> writeIORef s exprs >> c green >> d exprs
+
+draw :: DrawingArea -> [Expr] -> IO ()
+draw da exprs = do
+  win    <- widgetGetDrawWindow da
+  (w, h) <- widgetGetSize da
   drawWindowBeginPaintRect win $ Rectangle 0 0 w h
   renderWithDrawable win $ drawFretboard (realToFrac w) (realToFrac h) ebgdae exprs
   drawWindowEndPaint win

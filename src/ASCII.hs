5ae8d22800965f6ed3fa0e9d90a9bf10fdc26f92
daoo <daoo314@gmail.com>
2013-12-28 14:22:52 +0100

    Use bytestring builders

--- src/ASCII.hs
+++ src/ASCII.hs
@@ -1,10 +1,12 @@
 module Main (main) where
 
 import Data.Attoparsec.Char8
+import Data.ByteString.Lazy.Builder (hPutBuilder)
 import Fretboarder.Drawing.ASCII
 import Fretboarder.Music.Fretboard
 import Fretboarder.Parser
 import System.Environment
+import System.IO
 import qualified Data.ByteString.Char8 as B
 
 main :: IO ()
@@ -13,4 +15,4 @@ main = getArgs >>= prg
 prg :: [String] -> IO ()
 prg args = case parseOnly parseExprs (B.pack $ unwords args) of
   Left err    -> print err
-  Right exprs -> putStrLn $ asciiFretboard 23 ebgdae exprs
+  Right exprs -> hPutBuilder stdout $ asciiFretboard 23 ebgdae exprs
--- src/Fretboarder/Drawing/ASCII.hs
+++ src/Fretboarder/Drawing/ASCII.hs
@@ -1,34 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Fretboarder.Drawing.ASCII (asciiFretboard) where
 
+import Data.ByteString.Lazy.Builder
+import Data.ByteString.Lazy.Builder.ASCII
 import Data.List
+import Data.Monoid
 import Fretboarder.Drawing.Expr
 import Fretboarder.Music.Fretboard
 import Fretboarder.Music.Note
 import Fretboarder.Music.SPN
 
-chars :: String
-chars = "123456789ABCDEF"
+buildSPN :: SPN -> Builder
+buildSPN (SPN o p) = case p of
+  Cn -> charUtf8 'C' <> intDec (fromIntegral o)
+  Cs -> charUtf8 'C' <> intDec (fromIntegral o) <> charUtf8 '#'
+  Dn -> charUtf8 'D' <> intDec (fromIntegral o)
+  Ds -> charUtf8 'D' <> intDec (fromIntegral o) <> charUtf8 '#'
+  En -> charUtf8 'E' <> intDec (fromIntegral o)
+  Fn -> charUtf8 'F' <> intDec (fromIntegral o)
+  Fs -> charUtf8 'F' <> intDec (fromIntegral o) <> charUtf8 '#'
+  Gn -> charUtf8 'G' <> intDec (fromIntegral o)
+  Gs -> charUtf8 'G' <> intDec (fromIntegral o) <> charUtf8 '#'
+  An -> charUtf8 'A' <> intDec (fromIntegral o)
+  As -> charUtf8 'A' <> intDec (fromIntegral o) <> charUtf8 '#'
+  Bn -> charUtf8 'B' <> intDec (fromIntegral o)
 
-getChars :: Note -> [Expr] -> String
+chars :: [Builder]
+chars = map charUtf8 "123456789ABCDEF"
+
+getChars :: Note -> [Expr] -> [Builder]
 getChars n = map (chars !!) . semiIndex n
 
 -- TODO: Use some bytestring builder
-asciiFretboard :: Int -> Fretboard -> [Expr] -> String
-asciiFretboard c fb exprs = intercalate "\n" $ map string $ tuning fb
+asciiFretboard :: Int -> Fretboard -> [Expr] -> Builder
+asciiFretboard c fb exprs = foldl' (\a n -> a <> string n) mempty $ tuning fb
   where
-    string :: Note -> String
-    string root = nut root ++ nutNote root ++ "|" ++ frets root ++ "|"
+    string :: Note -> Builder
+    string root = nut root <> nutNote root <> pipe <> frets root <> pipe <> newline
 
-    nut :: Note -> String
-    nut n = show (fromSemi n :: SPN) ++ " |"
+    nut :: Note -> Builder
+    nut n = buildSPN (fromSemi n) <> space <> pipe
 
     nutNote n = case getChars n exprs of
-      []    -> "|"
-      (x:_) -> [x]
+      []    -> pipe
+      (x:_) -> x
 
-    frets :: Note -> String
-    frets root = intercalate "|" $ map fret [root + toEnum 1 .. root + toEnum c]
+    frets :: Note -> Builder
+    frets root = foldl' (\a n -> a <> pipe <> fret n) mempty [root + toEnum 1 .. root + toEnum c]
 
     fret n = case getChars n exprs of
-      []    -> "---"
-      (x:_) -> ['-', x, '-']
+      []    -> dash <> dash <> dash
+      (x:_) -> dash <> x <> dash
+
+    dash    = char8 '-'
+    newline = char8 '\n'
+    pipe    = char8 '|'
+    space   = char8 ' '
